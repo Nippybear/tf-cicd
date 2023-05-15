0b684930b85afc0d2459c3693ba843d0d17df018
Den Mark Medina <102699675+Nippybear@users.noreply.github.com>
2023-05-15 11:25:02 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,11 +15,7 @@ terraform {
 # Provider Block
 
 provider "azurerm" {
-    version         =   "~> 2.0"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
+    
     
     features {}
 }
